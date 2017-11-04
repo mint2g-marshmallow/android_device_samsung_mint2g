af45c6892c9d7ccabfada591bc2497f5558f0bc1
Cswl Coldwind <cswl1337@gmail.com>
2017-11-04 21:39:42 +0545

    board: kernel: use lineage defconfig, and support custom toolchain for building
    Change-Id: Icafaf3ce8f7d6f431a92bb26b89702efb7627acc

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,8 +3,11 @@ BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/mint2g
-TARGET_KERNEL_CONFIG := cyanogenmod_mint_defconfig
+TARGET_KERNEL_CONFIG := lineage-15_mint2g_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image
+KERNEL_HAS_FINIT_MODULE := false
+
+ifeq ($(KERNEL_TOOLCHAIN),)
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
-KERNEL_HAS_FINIT_MODULE := false
+endif
